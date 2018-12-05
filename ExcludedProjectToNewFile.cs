8b8dad9d5ff8a7e3d76ebf81dfa03a19c4b8c341
Mateusz Jankowski <mj@tek-projekt.pl>
2018-12-05 22:52:59 +0100

    init

---
+++ ExcludedProjectToNewFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace matjanko.RevitExtension
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Automatic)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+
+    public class ExcludedProjectToNewFile : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                TaskDialog.Show("test", "test");
+                return Result.Succeeded;
+            }
+            catch (Exception)
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
